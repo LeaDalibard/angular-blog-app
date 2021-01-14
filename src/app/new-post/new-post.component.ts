2f53a842fdc854fe30d9e1e43fe45313d37af6c4
Lea Dalibard <leadalibard@gmail.com>
2021-01-14 10:45:13 +0100

    Added a function onSavePost

--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {PostsService} from '../services/posts.service';
 import {Router} from '@angular/router';
+import {Post} from '../models/post.model';
 
 @Component({
   selector: 'app-new-post',
@@ -24,6 +25,17 @@ export class NewPostComponent implements OnInit {
       title: ['', Validators.required],
       content: ['', Validators.required],
     });
+  }
+
+  onSavePost() {
+    const title = this.postForm.get('title').value;
+    const content = this.postForm.get('content').value;
+    const newPost = new Post(title, content);
+    newPost.createdAt = new Date();
+    newPost.hateIts = 0;
+    newPost.loveIts = 0;
+    this.postsServices.addPost(newPost);
+    this.router.navigate(['/posts']);
   }
 
 }
